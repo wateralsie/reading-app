3e73d91defb7f8d372962eadae1024aa6303b764
waterminn <tnals08019@gmail.com>
2023-02-21 00:56:14 +0900

    [fix] di 제거

--- app/src/main/java/com/sumin/readroad/presentation/review/ReviewDetailActivity.kt
+++ app/src/main/java/com/sumin/readroad/presentation/review/ReviewDetailActivity.kt
@@ -1,13 +1,10 @@
 package com.sumin.readroad.presentation.review
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import com.sumin.readroad.R
 import com.sumin.readroad.databinding.ActivityReviewDetailBinding
 import com.sumin.readroad.util.binding.BindingActivity
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
-class ReviewDetailActivity: BindingActivity<ActivityReviewDetailBinding>(R.layout.activity_review_detail) {
+class ReviewDetailActivity :
+    BindingActivity<ActivityReviewDetailBinding>(R.layout.activity_review_detail) {
 
 }
