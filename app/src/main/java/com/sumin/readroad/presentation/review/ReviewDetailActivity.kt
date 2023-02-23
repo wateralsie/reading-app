cda79694d35ccf06b2be7646cc08e280ee73066d
waterminn <tnals08019@gmail.com>
2023-02-23 21:05:02 +0900

    [feat] 구문 저장 기능 구현

--- app/src/main/java/com/sumin/readroad/presentation/review/ReviewDetailActivity.kt
+++ app/src/main/java/com/sumin/readroad/presentation/review/ReviewDetailActivity.kt
@@ -1,10 +1,48 @@
 package com.sumin.readroad.presentation.review
 
+import android.view.ActionMode
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.viewModels
 import com.sumin.readroad.R
 import com.sumin.readroad.databinding.ActivityReviewDetailBinding
 import com.sumin.readroad.util.binding.BindingActivity
+import com.sumin.readroad.util.extension.showSnackbar
 
 class ReviewDetailActivity :
     BindingActivity<ActivityReviewDetailBinding>(R.layout.activity_review_detail) {
 
+    override fun constructLayout() {
+        binding.tvDetailContent.customSelectionActionModeCallback = disableMenu
+    }
+
+    override fun addListeners() {
+        binding.btnDetailScrap.setOnClickListener {
+            val content = binding.tvDetailContent
+            val selection =
+                binding.tvDetailContent.text.toString()
+                    .substring(content.selectionStart, content.selectionEnd).trim()
+            it.showSnackbar(selection)
+        }
+    }
+
+    private val disableMenu = object : ActionMode.Callback {
+        override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+            binding.btnDetailScrap.isEnabled = true
+            return true
+        }
+
+        override fun onPrepareActionMode(mode: ActionMode?, menu: Menu): Boolean {
+            menu.clear()
+            return true
+        }
+
+        override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?): Boolean {
+            return true
+        }
+
+        override fun onDestroyActionMode(mode: ActionMode?) {
+            binding.btnDetailScrap.isEnabled = false
+        }
+    }
 }
